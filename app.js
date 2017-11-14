df15a4e40e8014af255e09d8c72a2547affc0eda
p.jmoran <p.jmoran@icloud.com>
2017-11-14 17:22:52 -0500

    geo

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ var http = require('http'),
     bodyParser = require('body-parser'),
     fs = require('fs'),
     js2xmlparser = require('js2xmlparser'),
-    libxslt = require('libxslt');
+    libxslt = require('libxslt'),
+    ejs=require("ejs"),
+    geocoder=require("google-geocoder");
 
 var router = express();
 var server = http.createServer(router);
+var geo = geocoder({
+  key:"AIzaSyCOtY3B9nrFWLW699A8i6V5IJQ_TKwmgZo" 
+});
 
+router.set('view engine','ejs')
 router.use(express.static(path.resolve(__dirname, 'views')));
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
 // GET request to dislay index.html located inside /views folder
 router.get('/', function(req, res) {
-  res.render('index');
+  var json = fs.readFileSync('Teams.json','utf8');
+  var jsonParsed = JSON.parse(json);
+  
+  res.render('index',jsonParsed);
 });
 
 // HTML produced by XSL Transformation
@@ -35,41 +44,56 @@ router.get('/get/html', function(req, res) {
     res.end(result.toString());
   
 });
+router.get('/json/get',function(req,res){
+  res.writeHead(200, { 'Content-Type': 'application/json'});
+  var json = fs.readFileSync('Teams.json','utf8');
+  var jsonParsed = JSON.parse(json);
+  res.end(JSON.stringify(jsonParsed));
+})
 
 // POST request to add to JSON & XML files
 router.post('/post/json', function(req, res) {
 
   // Function to read in a JSON file, add to it & convert to XML
   function appendJSON(obj) {
+    
+     geo.find(obj.Location,function(err, response){
+       // Read in a JSON file
+        var JSONfile = fs.readFileSync('Teams.json', 'utf8');
 
-    // Read in a JSON file
-    var JSONfile = fs.readFileSync('Teams.json', 'utf8');
+        // Parse the JSON file in order to be able to edit it 
+        var JSONparsed = JSON.parse(JSONfile);
 
-    // Parse the JSON file in order to be able to edit it 
-    var JSONparsed = JSON.parse(JSONfile);
+        // Add a new record into team array within the JSON file
+        obj.coordinates = response;
+        JSONparsed.team.push(obj);
 
-    // Add a new record into team array within the JSON file    
-    JSONparsed.team.push(obj);
+        // Beautify the resulting JSON file
+        var JSONformated = JSON.stringify(JSONparsed, null, 4);
 
-    // Beautify the resulting JSON file
-    var JSONformated = JSON.stringify(JSONparsed, null, 4);
+        // Write the updated JSON file back to the system 
+        fs.writeFileSync('Teams.json', JSONformated);
 
-    // Write the updated JSON file back to the system 
-    fs.writeFileSync('Teams.json', JSONformated);
+        // Convert the updated JSON file to XML     
+        var XMLformated = js2xmlparser.parse("myTeams", JSON.parse(JSONformated));
 
-    // Convert the updated JSON file to XML     
-    var XMLformated = js2xmlparser.parse("myTeams", JSON.parse(JSONformated));
+        // Write the resulting XML back to the system
+        fs.writeFileSync('Teams.xml', XMLformated);
+          // Re-direct the browser back to the page, where the POST request came from
+       res.redirect('/');
+      
+    })
 
-    // Write the resulting XML back to the system
-    fs.writeFileSync('Teams.xml', XMLformated);
-
+    
+    
+    
+   
   }
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
   
-  // Re-direct the browser back to the page, where the POST request came from
-  res.redirect('back');
+  
 
 });
 
